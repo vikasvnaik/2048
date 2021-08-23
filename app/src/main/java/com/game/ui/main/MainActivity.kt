ab9916e7043f08c6f039f1b5d90c042510c55b5e
vikas <vikas.naik@transo.in>
2021-08-23 11:12:39 +0530

    * cleaning code.

--- app/src/main/java/com/game/ui/main/MainActivity.kt
+++ app/src/main/java/com/game/ui/main/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import com.game.ui.base.BaseAppCompatActivity
 import com.game.databinding.ActivityMainBinding
 import com.game.extensions.viewBinding
+import com.game.ui.views.GameView
 
 class MainActivity : BaseAppCompatActivity(){
+
     private val binding by viewBinding(ActivityMainBinding::inflate)
     override fun layout() = binding.root
+    private var view: GameView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        view = GameView(this, userDataManager)
+        binding.flGame.addView(view)
     }
 
     override fun onPause() {
